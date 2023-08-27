07434c0d62ba3a9cfebfc312b5a554e9b82b8fb9
xavier-kong <kongxianchengxavier@gmail.com>
2023-08-27 11:26:32 +0800

    more css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import './App.css'
+import { useState } from 'react';
+import LeftRightToggle from './components/LeftRightToggle';
 
 function App() {
+    const [direction, setDirection] = useState<'row' | 'col'>('row');
 
-  return (
-    <>
 
-    </>
-  )
+    return (
+        <>
+            <div className="main">
+                <div className="title-container">
+                    <p className="title">Flex Ratio Calculator</p>
+                </div>
+                <div className="toolbar">
+                    <div className="toggle-container" style={{ justifyContent: 'left'}}>
+                        <div className="toggle-circle" />
+                    </div>
+                </div>
+                <div className="box-container">
+                    {
+                        direction === 'row'
+                            ? <div className="row-div"></div>
+                            : <div className="col-div"></div>
+                    }
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
