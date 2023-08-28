4e1172786e93da2bc970b1d5cfe3d3d28b02c907
xavier-kong <kongxianchengxavier@gmail.com>
2023-08-28 08:57:27 +0800

    added toggle with transitions and div count skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,62 @@
 import './App.css'
 import { useState } from 'react';
-import LeftRightToggle from './components/LeftRightToggle';
 
 function App() {
-    const [direction, setDirection] = useState<'row' | 'col'>('row');
+    const [direction, setDirection] = useState<boolean>(false); // left is false, right is true
+    const [divCount, setDivCount] = useState<number>(1);
 
+    const handleDivCount = (offset: number) => {
+        const currCount = divCount;
+        let processedDivCount = currCount + (offset);
+        if (processedDivCount <= 0) { processedDivCount = 0; }
+        if (processedDivCount >= 10) { processedDivCount = 10; }
+        setDivCount(processedDivCount);
+    }
 
     return (
         <>
-            <div className="main">
-                <div className="title-container">
+            <div className="main flex-col-centered">
+                <div className="flex-1">
                     <p className="title">Flex Ratio Calculator</p>
                 </div>
-                <div className="toolbar">
-                    <div className="toggle-container" style={{ justifyContent: 'left'}}>
-                        <div className="toggle-circle" />
+                <div className="flex-1 flex-row-centered toolbar">
+                    <div className="flex-1 flex-col-centered">
+                        <div className="flex-1 flex-col-centered">
+                            <p className="tool-header-text">Flex Direction</p>
+                        </div>
+                        <div className="tool-body flex-row-centered">
+                            <div className="flex-1" style={{ textAlign: 'right' }}>
+                                <p className="toggle-label">Row</p>
+                            </div>
+                            <div className="toggle-button-container flex-row-centered" onClick={() => setDirection(!direction)}>
+                                <div className={`toggle-circle ${!direction ? 'slideLeft' : 'slideRight'}`} />
+                            </div>
+                            <div className="flex-1">
+                                <p className="toggle-label">Column</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex-1 flex-col-centered">
+                        <div className="flex-1 flex-col-centered">
+                            <p className="tool-header-text">Div Count</p>
+                        </div>
+                        <div className="tool-body flex-row-centered">
+                            <div className="count-button" onClick={() => handleDivCount(-1)}>
+                                <p>-</p>
+                            </div>
+                            <div className="flex-col-centered" style={{ flex: 2, width: 40, textAlign: 'center' }}>
+                                {divCount}
+                            </div>
+                            <div className="count-button" onClick={() => handleDivCount(1)}>
+                                <p>+</p>
+                            </div>
+
+                        </div>
+
                     </div>
                 </div>
                 <div className="box-container">
-                    {
-                        direction === 'row'
-                            ? <div className="row-div"></div>
-                            : <div className="col-div"></div>
-                    }
+                    <div className={!direction ? 'row-div' : 'col-div'}></div>
                 </div>
             </div>
         </>
